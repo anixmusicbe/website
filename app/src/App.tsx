a2eb7ab63e18431f58a3abfd39650be877ce074f
Ingo Andelhofs <ingoandelhofs@gmail.com>
2025-05-15 19:14:41 +0200

    :sparkles: Created simple website for anix

---
+++ app/src/App.tsx
@@ -0,0 +1,8 @@
+import logo from '../public/anix_logo.svg';
+
+export default function App() {
+
+	return <div className="w-full h-dvh flex items-center justify-center bg-black">
+		<img className="max-w-[200px] w-[8/12]" src={logo} alt="anix_logo" />
+	</div>
+}
