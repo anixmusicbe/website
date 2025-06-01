b651cf92775179d24d6f39638670706dc88552fa
Ingo Andelhofs <ingoandelhofs@gmail.com>
2025-06-01 23:02:21 +0200

    :sparkles: Added analytics with logsnag

--- link/src/components/logsnag/Visitor.tsx
+++ link/src/components/logsnag/Visitor.tsx
@@ -8,12 +8,16 @@ export default function Visitor() {
 	const logSnag = useLogSnag();
 
 	useEffect(() => {
-		logSnag.track({
-			event: 'visit',
-			channel: 'visitors',
-			description: 'Someone has visited your website',
-		});
-	}, []);
+		const timer = setTimeout(() => {
+			logSnag.track({
+				event: 'visit',
+				channel: 'visitors',
+				description: 'Someone has visited your website',
+			});
+		}, 1000);
+
+		return () => clearTimeout(timer);
+	}, []); // Empty dependency array ensures it only runs once
 
 	return null;
 }
