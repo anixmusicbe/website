be44cec8142e11860785e2f4e1758a072cc4c521
Ingo Andelhofs <ingoandelhofs@gmail.com>
2025-12-07 22:25:26 +0100

    :bug: Bugfix

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,13 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		tailwindcss()
+		tailwindcss(),
+		netlifyReactRouter(),
 	],
 })
